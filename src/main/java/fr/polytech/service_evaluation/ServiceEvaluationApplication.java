49d753ce927c4b837491fcd0f3f26b1e15c2be9e
Vincent Dubuc <cerise.litchis.0z@icloud.com>
2024-11-23 22:17:25 +0100

    Init project

---
+++ src/main/java/fr/polytech/service_evaluation/ServiceEvaluationApplication.java
@@ -0,0 +1,13 @@
+package fr.polytech.service_evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceEvaluationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/polytech/service_evaluation/ServiceEvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.polytech.service_evaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
